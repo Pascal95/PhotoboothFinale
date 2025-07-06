c21afe18bf80ce1c00857e4b9ed4f41a8ef84800
Pierre-Richard Pascal <89923424+Pascal95@users.noreply.github.com>
2025-07-06 18:22:42 +0200

    no conversion

--- utils/impression.py
+++ utils/impression.py
@@ -1,13 +1,12 @@
-from PIL import Image
 import subprocess
+import os
 
 def imprimer_image(path_image, nom_imprimante="Canon_SELPHY_CP1500"):
     try:
-        image = Image.open(path_image)
-        image = image.convert("RGB")  # au cas où c’est JPEG
-        temp_path = "print_ready.png"
-        image.save(temp_path, format="PNG")
-        subprocess.run(["lp", "-d", nom_imprimante, temp_path], check=True)
+        if not os.path.exists(path_image):
+            print(f"❌ Fichier non trouvé : {path_image}")
+            return
+        subprocess.run(["lp", "-d", nom_imprimante, path_image], check=True)
         print("✅ Impression lancée avec succès.")
     except Exception as e:
         print(f"❌ Erreur pendant l’impression : {e}")
